2024b63a63648e1a00c98fb935c23864aa82620f
Vukadin Lazarevic <vukadinlazarevic@gmail.com>
2025-01-04 00:59:15 +0100

    added extra div for good measure

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,12 @@ import {SpeedInsights} from "@vercel/speed-insights/vue";
 </script>
 
 <template>
-  <SpeedInsights />
-  <div
-    :class="'min-h-screen flex flex-col items-center justify-center text-center bg-gray-100 dark:bg-gray-900 md:p-2 sm:p-2'">
-    <RouterView />
+  <div>
+    <SpeedInsights />
+    <div
+      :class="'min-h-screen flex flex-col items-center justify-center text-center bg-gray-100 dark:bg-gray-900 md:p-2 sm:p-2'">
+      <RouterView />
+    </div>
   </div>
 </template>
 
