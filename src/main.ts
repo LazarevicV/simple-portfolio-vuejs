c416cc17813331a8dc13328caf776f940cf57a4e
Vukadin Lazarevic <vukadinlazarevic@gmail.com>
2025-01-04 00:49:13 +0100

    added speedInsights for vercel from correct path

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 
 const app = createApp(App)
 
+app.use(SpeedInsights)
 app.use(router)
 
 app.mount('#app')
