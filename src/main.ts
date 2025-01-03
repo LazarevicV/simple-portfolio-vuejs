52d83bd597d71e717be518b92add008ae624411b
Vukadin Lazarevic <vukadinlazarevic@gmail.com>
2025-01-04 00:42:01 +0100

    added speedInsights for vercel

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const app = createApp(App)
 
