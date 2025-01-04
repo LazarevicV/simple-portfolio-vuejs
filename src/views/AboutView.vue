2edd2152b8a8e68476abde34a4ad28c233b7551a
Vukadin Lazarevic <vukadinlazarevic@gmail.com>
2025-01-04 01:05:03 +0100

    fixed text and button on about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div>
     <h1 class="text-slate-100 text-5xl font-bold mb-10">This is an about page</h1>
     <RouterLink
       class="text-white w-48 rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-lg font-medium transition-all shadow-md hover:shadow-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-900 active:bg-blue-800 disabled:pointer-events-none disabled:opacity-50"
@@ -8,15 +8,3 @@
     </RouterLink>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
-<script setup lang="ts">
-</script>
